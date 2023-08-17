8b3ddc42a40874ce84c28900d4fe90381753cdaa
ZYX <zyxemail.24@gmail.com>
2023-08-17 16:55:04 +0700

    1

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// Setup environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
 // Server configuration
 export const SERVER_PORT = 3000; // Server port
 export const DEBUG = false; // Debug mode
@@ -16,8 +20,8 @@ export const WHITELISTED_IPS = [
 
 // OpenAI API Keys
 export let OPENAI_KEYS = [
-    "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",
-    "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",
-    "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",
-    "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",
+    process.env.OPENAI_1,
+    process.env.OPENAI_2,
+    process.env.OPENAI_3,
+    process.env.OPENAI_4,
 ];
